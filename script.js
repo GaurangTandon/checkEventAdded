8fe597ce473a7cfb4da0ec96b14d364000fd63ec
Gaurang Tandon <1gaurangtandon@gmail.com>
2015-06-10 15:51:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-// override add/removeEventListener method to
-// keep track of all added events to a element
-(function(window){
-	function makeListener(name, bool){
-		var list = {}, f = EventTarget.prototype[name + "EventListener"];
-		
-		return function(type, callback, capture, cb1, cb2){
-			// event has already been added
-			// do not attach listener
-			if(list[type] == bool || (!bool && !list[type])){
-				cb2(); return false;
-			}
+var hasEvent, getEvents;
+(function (window) {
+    hasEvent = function (elm, type) {
+        var ev = elm.dataset.events;
+        if (!ev) return false;
 
-			f.call(this, type, callback, capture);				
-			list[type] = bool;
-			cb1();
-			
-			return true;
-		};
-	}
-	
-	EventTarget.prototype.addEventListener = makeListener("add", true);
-	EventTarget.prototype.removeEventListener = makeListener("remove", false);
+        return (new RegExp(type)).test(ev);
+    };
+
+    getEvents = function (elm) {
+        return elm.dataset.events.replace(/(^,+)|(,+$)/g, "").split(",").filter(function (elm) {
+            return elm !== "";
+        });;
+    };
+
+    function addRemoveEvent(elm, type, bool) {
+        if (bool) elm.dataset.events += "," + type;
+        else elm.dataset.events = elm.dataset.events.replace(new RegExp(type), "");
+    }
+
+    function makeListener(name, bool) {
+        var f = EventTarget.prototype[name + "EventListener"];
+
+        return function (type, callback, capture, cb1, cb2) {
+            if (!this.dataset.events) this.dataset.events = "";
+
+            var has = hasEvent(this, type);
+
+            // event has already been added/removed
+            // do not attach listener
+            if ((bool && has) || (!bool && !has)) {
+                if (cb2) cb2();
+                return false;
+            }
+
+            f.call(this, type, callback, capture);
+            addRemoveEvent(this, type, bool);
+
+            if (cb1) cb1();
+
+            return true;
+        };
+    }
+
+    EventTarget.prototype.addEventListener = makeListener("add", true);
+    EventTarget.prototype.removeEventListener = makeListener("remove", false);
 })();
